3511080ab92429cef583ee495be42762db211a57
Hossein Farahkordmahaleh <113180290+HosseinFarah@users.noreply.github.com>
2023-09-10 12:54:06 +0300

    useNavigate

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 export const Data = ()=>{
+    const navigate = useNavigate();
     const {id} = useParams()
     const selector = useSelector((state)=>state.form)
     return(
         <div className="alert alert-info">
         Data set with redux from the valid submitted form with useForm,yup,yupresolver
 {<h5>{selector.fname},{selector.lname},{selector.gender},{selector.age},{selector.password},{selector.email}</h5>}
+        <button className="btn btn-success" onClick={()=>navigate("/")}>Back</button>
         </div>
     )
 }
